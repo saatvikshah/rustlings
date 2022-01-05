e7407bde0b4389dc26802722dd17717a99498404
sshah1 <saatvik.shah@imc.com>
2022-01-05 07:05:24 +0100

    Complete quiz1 (variables, fns, if)

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -8,10 +8,14 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_apple_price {
+fn calculate_apple_price(num_apples: u32) -> u32 {
+    const DISCOUNT_THRESHOLD: u32 = 40;
+    const APPLE_ORIG_PRICE: u32 = 2;
+    const APPLE_DISCOUNT_PRICE: u32 = 1;
+    let apple_price = if num_apples > DISCOUNT_THRESHOLD { APPLE_DISCOUNT_PRICE } else { APPLE_ORIG_PRICE };
+    apple_price * num_apples
+}
 
 // Don't modify this function!
 #[test]
